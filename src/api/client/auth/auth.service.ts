79ec8b634d6012399e6c0cfd6e26e22908a945ad
MilosGak97 <127430859+MilosGak97@users.noreply.github.com>
2024-12-12 08:54:15 -0500

    Initial commit

--- src/api/client/auth/auth.service.ts
+++ src/api/client/auth/auth.service.ts
@@ -132,7 +132,7 @@ export class AuthService {
             return { message: "Reset password url has been successfully sent if we've found matching email in our database." }
         }
         await this.tokenRepository.saveToken(user, forgotPasswordToken,TokenType.FORGOT_PASSWORD,'7d')
-        const forgotPasswordUrl =  `${process.env.BASE_URL}client/auth/forgot-password?${encodeURIComponent(forgotPasswordToken)}`
+        const forgotPasswordUrl =  `${process.env.BASE_URL}client/auth/forgot-password/${encodeURIComponent(forgotPasswordToken)}`
         await this.emailService.forgotPasswordEmail(user.email,forgotPasswordUrl,forgotPasswordToken)
 
         return {
