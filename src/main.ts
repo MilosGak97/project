de8a56a849419b1fd8a3601ed07a930e0995c33c
MilosGak97 <127430859+MilosGak97@users.noreply.github.com>
2025-01-03 18:57:46 -0500

    m

--- src/main.ts
+++ src/main.ts
@@ -82,7 +82,7 @@ async function bootstrap() {
 
 
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3001;
   await app.listen(PORT);
   console.log(`Application is running on: http://localhost:${PORT}`);
 }
