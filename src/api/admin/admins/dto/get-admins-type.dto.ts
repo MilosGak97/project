8110d0c93c452712cf67f0a6207a1f6b2d04401e
MilosGak97 <127430859+MilosGak97@users.noreply.github.com>
2025-01-03 19:33:12 -0500

    m

--- src/api/admin/admins/dto/get-admins-type.dto.ts
+++ src/api/admin/admins/dto/get-admins-type.dto.ts
@@ -1,9 +1,8 @@
 
-import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
+import {  ApiProperty } from '@nestjs/swagger';
 import { AdminRole } from '../../../enums/admin-role.enum';
 import { AdminStatus } from '../../../enums/admin-status.enum';
 
-@ApiExtraModels(GetAdminsTypeDto)
 export class GetAdminsTypeDto {
   @ApiProperty()
   id: string;
