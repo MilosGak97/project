66e2925ff17eadc177e09e27fb112bd864f40833
MilosGak97 <127430859+MilosGak97@users.noreply.github.com>
2025-02-06 16:38:59 -0500

    m

--- src/api/admin/companies/dto/update-company.dto.ts
+++ src/api/admin/companies/dto/update-company.dto.ts
@@ -1,39 +1,36 @@
 import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from "class-validator";
-import { AddressDto } from "./address.dto";
+import { IsOptional, IsString } from 'class-validator';
+import { AddressDto } from './address.dto';
 
 @ApiExtraModels(AddressDto)
 export class UpdateCompanyDto {
-    
-    @ApiProperty({required:false})
-    @IsOptional()
-    @IsString()
-    name?:string
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  name?: string;
 
-    @ApiProperty({required:false})
-    @IsOptional()
-    @IsString()
-    phoneNumber?:string
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  phoneNumber?: string;
 
-    @ApiProperty({required:false})
-    @IsOptional()
-    @IsString()
-    email?:string
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  email?: string;
 
-    @ApiProperty({required:false})
-    @IsOptional()
-    @IsString()
-    website?:string
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  website?: string;
 
-    @ApiProperty({required:false})
-    @IsOptional()
-    @IsString()
-    logoUrl?:string
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  logoUrl?: string;
 
-    @ApiProperty({required:false, enum:AddressDto})
-    @IsOptional()
-    @IsString()
-    address?:AddressDto
-
-    
+  @ApiProperty({ required: false, type: AddressDto })
+  @IsOptional()
+  @IsString()
+  address?: AddressDto;
 }
