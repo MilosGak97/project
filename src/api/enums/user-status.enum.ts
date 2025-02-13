e6944908a288c80ac65bddd0f6a733b643f6f90b
MilosGak97 <127430859+MilosGak97@users.noreply.github.com>
2025-02-13 13:55:44 -0500

    M

--- src/api/enums/user-status.enum.ts
+++ src/api/enums/user-status.enum.ts
@@ -2,7 +2,6 @@ export enum UserStatus {
     DELETED = 'DELETED',
     SCHEDULED_DELETE = 'SCHEDULED_DELETE',
     ACTIVE = 'ACTIVE', // After setting up the password
-    PAUSED = 'PAUSED',
     SUSPENDED = 'SUSPENDED', // Email is verified, password setted up, but suspended
     NO_PASSWORD = 'NO_PASSWORD', // Email is verified but password is not setted up yet
     NEW_REGISTER = 'NEW_REGISTER', // User is registered, but email not verified
